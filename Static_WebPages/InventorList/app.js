87ac1f35ec89cbc03b813b0ceea32d984a74ada8
Preetham Reddy <preethamreddy@Preethams-MacBook-Air.local>
2025-12-03 12:05:22 +0530

    Updated files

--- Static_WebPages/InventorList/app.js
+++ Static_WebPages/InventorList/app.js
@@ -1,49 +1,59 @@
-const inventors = {
-    "C": { name: "Dennis Ritchie", image: "images/ritchie.jpg" },
-    "C++": { name: "Bjarne Stroustrup", image: "images/stroustrup.jpg" },
-    "Java": { name: "James Gosling", image: "images/gosling.jpg" },
-    "Python": { name: "Guido van Rossum", image: "images/rossum.jpg" },
-    "JavaScript": { name: "Brendan Eich", image: "images/eich.jpg" },
-    "PHP": { name: "Rasmus Lerdorf", image: "images/lerdorf.JPG" },
-    "Ruby": { name: "Yukihiro Matsumoto", image: "images/matsumoto.jpg" },
-    "Lisp": { name: "John McCarthy", image: "images/mccarthy.jpg" },
-    "COBOL": { name: "Grace Hopper", image: "images/hopper.jpg" },
-    "C#": { name: "Anders Hejlsberg", image: "images/hejlsberg.jpg" }
-};
+function showInventor() {
+    const inventor = document.getElementById("inventorList").value;
 
-document.addEventListener('DOMContentLoaded', () => {
-    const languageSelect = document.getElementById('languageSelect');
-    const resultDisplay = document.getElementById('resultDisplay');
-
-    languageSelect.addEventListener('change', (e) => {
-        const language = e.target.value;
-        if (language && inventors[language]) {
-            displayInventor(language, inventors[language]);
+    const data = {
+        gosling: {
+            img: "images/gosling.jpg",
+            info: "James Gosling is the creator of Java, developed at Sun Microsystems in 1995."
+        },
+        rossum: {
+            img: "images/rossum.jpg",
+            info: "Guido van Rossum created Python in 1991 and is known as the 'Benevolent Dictator For Life' of Python."
+        },
+        ritchie: {
+            img: "images/ritchie.jpg",
+            info: "Dennis Ritchie created the C programming language at Bell Labs in 1972."
+        },
+        stroustrup: {
+            img: "images/stroustrup.jpg",
+            info: "Bjarne Stroustrup developed C++ as an extension of C beginning in 1979."
+        },
+        eich: {
+            img: "images/eich.jpg",
+            info: "Brendan Eich created JavaScript in 1995 while working at Netscape."
+        },
+        lerdorf: {
+            img: "images/lerdorf.JPG",
+            info: "Rasmus Lerdorf created PHP in 1994 originally as CGI scripts."
+        },
+        matsumoto: {
+            img: "images/matsumoto.jpg",
+            info: "Yukihiro 'Matz' Matsumoto created Ruby in 1995 focusing on simplicity and productivity."
+        },
+        hejlsberg: {
+            img: "images/hejlsberg.jpg",
+            info: "Anders Hejlsberg developed C# for Microsoft in 2000."
+        },
+        hopper: {
+            img: "images/hopper.jpg",
+            info: "Grace Hopper created COBOL and pioneered compiler technology."
+        },
+        mccarthy: {
+            img: "images/mccarthy.jpg",
+            info: "John McCarthy created Lisp in 1958 and is considered one of the founders of AI."
         }
-    });
+    };
 
-    function displayInventor(language, data) {
-        resultDisplay.style.opacity = '0';
-        
-        setTimeout(() => {
-            let imageHtml = '';
-            if (data.image) {
-                imageHtml = `
-                    <div class="image-container">
-                        <img src="${data.image}" alt="${data.name}" class="inventor-photo">
-                    </div>
-                `;
-            }
+    const imgElement = document.getElementById("inventorImage");
+    const infoElement = document.getElementById("infoBox");
 
-            resultDisplay.innerHTML = `
-                <div class="inventor-card">
-                    <div class="inventor-label">Inventor of</div>
-                    <div class="language-name">${language}</div>
-                    ${imageHtml}
-                    <div class="inventor-name">${data.name}</div>
-                </div>
-            `;
-            resultDisplay.style.opacity = '1';
-        }, 200);
+    if (inventor === "") {
+        imgElement.src = "";
+        imgElement.style.display = "none";
+        infoElement.innerText = "Select an inventor to view details";
+    } else {
+        imgElement.src = data[inventor].img;
+        imgElement.style.display = "block";
+        infoElement.innerText = data[inventor].info;
     }
-});
+}
